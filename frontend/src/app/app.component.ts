ac68ebff9d51e209934a34941ad6ad5eeef3a073
dianacah <forest.carolinadh@gmail.com>
2019-11-27 22:53:08 -0500

    Creación del servicio y llamado correcto en consola

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//Importando el servicio
+import { WeatherService } from './services/weather.service'
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weather-cannvest';
+// export class AppComponent {
+//   title = 'weather-cannvest';
+// }
+
+export class AppComponent implements OnInit {
+
+constructor(private weatherService: WeatherService){
+
+}
+//imprimiendo en consola listo para un placeholdeer
+  ngOnInit(){
+    this.weatherService.getWeather('london')
+    .subscribe(
+      res => console.log(res),
+      err => console.log(err)
+    )
+  }
 }
